b7cb4abf36c71c3517766e1ef1f627a096091972
Jay Olson <jay.olson@sinclair.edu>
2025-07-14 21:25:41 -0400

    Set up initial skeleton for driver and character function prototype. Need to add try/catch.

--- primary.cpp
+++ primary.cpp
@@ -3,14 +3,40 @@
 //July 14, 2025
 //Unit 8 Exceptions Part A
 
+//This program has a function to calculate character offsets, throwing exceptions in error conditions.
+
 #include <iostream>
 using namespace std;
 
+char character(char, int);
+
 int main() {
+
+	char userChar = '$';
+	int userOffset = -1;
+
+	cout << "Enter a character (A-Z or a-z): ";
+	cin >> userChar;
+
+	cout << "Enter an offset value: ";
+	cin >> userOffset;
+
+	character(userChar, userOffset);
 
 
 	cout << endl << endl;
 	system("pause");
 	return 0;
+
+}
+
+
+char character(char start, int offset) {
+
+	try {
+
+
+
+	}
 
 }
