d184f5ef66f97a17813b57aca000cde9ee400393
Jay Olson <jay.olson@sinclair.edu>
2025-07-14 21:12:14 -0400

    Add project files.

---
+++ primary.cpp
@@ -0,0 +1,16 @@
+//Jay Olson
+//CIS 1202-800
+//July 14, 2025
+//Unit 8 Exceptions Part A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+
+}
